e76416cd985289855540b197e900b1dbf2210f70
Dimitris Trechas <dimitristrechas@gmail.com>
2024-01-12 18:47:58 +0200

    Add new script for rails console

--- scripts.sh
+++ scripts.sh
@@ -6,6 +6,7 @@ echo -e " (1) to start the development server with a fresh build"
 echo -e " (2) to start the development server"
 echo -e " (3) to start the tailwindcss:watch service"
 echo -e " (4) to delete server.pid"
+echo -e " (5) to open rails console in docker"
 read OPTION
 
 case $OPTION in
@@ -24,6 +25,10 @@ case $OPTION in
 
     4)
     rm tmp/pids/server.pid
+    ;;
+
+    5)
+    docker compose exec skateparks bundle exec rails console
     ;;
 
     *)
