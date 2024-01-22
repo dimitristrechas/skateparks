6972d04d96a11cbeaf7a79965db6c63c0a435231
Dimitris Trechas <dimitristrechas@gmail.com>
2024-01-22 08:54:53 +0200

    Update sitemap.rb

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,13 +3,6 @@ require 'aws-sdk-s3'
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.skateparks.gr"
 
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new('skateparks.gr',
-  acl: 'public-read', # Optional. This is the default.
-  cache_control: 'private, max-age=0, no-cache', # Optional. This is the default.
-  access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-  secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-  region: 'eu-central-1'
-)
 
 SitemapGenerator::Sitemap.create do
 
