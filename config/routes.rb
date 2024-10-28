05590ed1d54cd961713eda9d454df8fc90206ce6
Dimitris Trechas <dimitristrechas@gmail.com>
2024-10-28 19:23:37 +0200

    Remove temp redirect for skatepark

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,6 @@ Rails.application.routes.draw do
     resources :skateparks
   end
 
-  if Rails.env.production?
-    # 301 permant redirects
-    get "/skateparks/16-xanthi" => redirect("/skateparks/21-xanthi", status: 301)
-  end
-
   resources :skateparks
 
   get "healthcheck" => "rails/health#show", as: :rails_health_check
