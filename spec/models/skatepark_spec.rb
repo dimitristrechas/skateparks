98ce65977c0eba616841bf24b2af245623ce7d6e
Dimitris Trechas <dimitristrechas@gmail.com>
2024-10-30 22:52:20 +0200

    Add slug and to_param specs for skatepark model

--- spec/models/skatepark_spec.rb
+++ spec/models/skatepark_spec.rb
@@ -89,5 +89,45 @@ RSpec.describe Skatepark, type: :model do
       skatepark.status = :published
       expect(skatepark.save).to be true
     end
+
+    it "generates a slug based on the name" do
+      skatepark = Skatepark.new(name_en: "Test Skatepark",
+                                name_el: "Test Skatepark",
+                                cover_image: fixture_file_upload('sample_image2.jpg'),
+                                lat: 12.345,
+                                lng: 67.890,
+                                description: "Skatepark description",
+                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
+                                status: :published)
+      skatepark.save
+      expect(skatepark.slug).to eq("test-skatepark")
+    end
+
+    it "updates the slug when the name changes" do
+      skatepark = Skatepark.new(name_en: "Test Skatepark",
+                                name_el: "Test Skatepark",
+                                cover_image: fixture_file_upload('sample_image2.jpg'),
+                                lat: 12.345,
+                                lng: 67.890,
+                                description: "Skatepark description",
+                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
+                                status: :published)
+      skatepark.save
+      skatepark.update(name_en: "Updated Name")
+      expect(skatepark.slug).to eq("updated-name")
+    end
+
+    it "returns the correct format in to_param" do
+      skatepark = Skatepark.create!(name_el: "Chicago Skate Park",
+                                    name_en: "Chicago Skate Park",
+                                    cover_image: fixture_file_upload('sample_image2.jpg'),
+                                    lat: 41.8819,
+                                    lng: -87.6231,
+                                    description: "Another skatepark option",
+                                    images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
+                                    status: :published)
+
+      expect(skatepark.to_param).to eq("#{skatepark.id}-chicago-skate-park")
+    end
   end
 end
