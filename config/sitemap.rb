0db49fc93c9bcc6d2393ba53555a252205689780
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-06-28 10:24:22 +0300

    feat: Move sitemap internally

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,19 +1,5 @@
-require 'aws-sdk-s3'
-
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.skateparks.gr"
-
-SitemapGenerator::Sitemap.public_path = 'tmp/'
-
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemap'
-
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new('skateparks.gr',
-  acl: 'public-read', # Optional. This is the default.
-  cache_control: 'private, max-age=0, no-cache', # Optional. This is the default.
-  access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-  secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-  region: 'eu-central-1'
-)
 
 SitemapGenerator::Sitemap.create do
 
@@ -24,4 +10,3 @@ SitemapGenerator::Sitemap.create do
     add skatepark_path(skatepark), :lastmod => skatepark.updated_at
   end
 end
- 
