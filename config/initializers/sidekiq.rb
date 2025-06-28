6dda9378bb8b40f867e823ce7ae3fe5ba94f5aa7
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-06-28 23:10:49 +0300

    feature: install sidekiq and add redis

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,15 @@
+schedule_file = "config/schedule.yml"
+
+redis_config = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
+end
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
--- config/schedule.rb
+++
@@ -1,3 +0,0 @@
-every 1.week, :at => '5:00 am' do
-  rake "-s sitemap:refresh"
-end
