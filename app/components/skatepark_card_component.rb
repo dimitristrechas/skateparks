32b20f8e2d364a6770e30728665730e716d4ce58
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-11-28 21:29:09 +0200

    fix: remove rotate on hover

--- app/components/skatepark_card_component.rb
+++ app/components/skatepark_card_component.rb
@@ -4,17 +4,4 @@ class SkateparkCardComponent < ViewComponent::Base
     @skatepark = skatepark
     @badge_type = badge_type
   end
-
-  private
-
-  def rotation_class
-    case @badge_type
-    when :new
-      'hover:rotate-5'
-    when :popular
-      'hover:-rotate-5'
-    else
-      ''
-    end
-  end
 end
