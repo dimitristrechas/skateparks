df9c4e6190e41eb308256806b56aaef4a63f8c04
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-12-13 23:33:52 +0200

    feat: Improve seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,33 @@ require 'open-uri'
 
 # Create sample skateparks
 10.times do |i|
-  country = ISO3166::Country.all.select { |c| c.subdivisions.any? }.sample
+  european_countries = ISO3166::Country.all.select { |c| c.continent == 'Europe' && c.subdivisions.any? }
+  country = european_countries.sample
+  subdivision = country.subdivisions.values.sample
+  suffix = i + rand(10)
+
+  subdivision_name = subdivision.name
+  geo = subdivision.geo || {}
+  lat = geo['latitude'] || 40.935657
+  lng = geo['longitude'] || 24.402491
+
   skatepark = Skatepark.new(
-    name_en: "Sample Skatepark #{i + rand(1000)}",
-    name_el: "Sample Skatepark #{i + rand(1000)}",
-    description_en: "This is a sample skatepark description for park #{i + 1}.",
-    description_el: "This is a sample skatepark description for park #{i + 1}.",
-    lat: 40.935657,
-    lng: 24.402491,
+    name_en: "#{subdivision_name} Skatepark #{suffix}",
+    name_el: "#{subdivision_name} Skatepark #{suffix}",
+    description_en: 'This is a sample skatepark description for park.',
+    description_el: 'Αυτή είναι μια περιγραφή για το skatepark.',
+    lat: lat,
+    lng: lng,
     status: :published,
     country_code: country.alpha2,
-    state: country.subdivisions.values.sample['code']
+    state: subdivision.code
   )
 
   cover_image_url = 'https://picsum.photos/200/300'
   cover_image = URI.open(cover_image_url)
   skatepark.cover_image.attach(
     io: cover_image,
-    filename: "skatepark#{i + rand(1000)}_cover.jpg",
+    filename: "skatepark#{i + rand(10)}_cover.jpg",
     content_type: 'image/jpeg'
   )
 
@@ -27,7 +36,7 @@ require 'open-uri'
     image = URI.open(cover_image_url)
     skatepark.images.attach(
       io: image,
-      filename: "skatepark#{i + rand(1000)}_image#{j + rand(1000)}.jpg",
+      filename: "skatepark#{i + rand(10)}_image#{j + rand(10)}.jpg",
       content_type: 'image/jpeg'
     )
   end
