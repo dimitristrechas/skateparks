f983d58186408af218c075c47f6489e1e7f7794f
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-12-13 23:54:39 +0200

    fix: auth on admin home page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 module Admin
   class DashboardController < ApplicationController
+    http_basic_authenticate_with name: Rails.application.credentials.dig(:admin, :username),
+                                 password: Rails.application.credentials.dig(:admin, :password),
+                                 unless: -> { Rails.env.development? }
+
     def index
     end
   end
