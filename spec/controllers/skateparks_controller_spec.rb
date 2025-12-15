9d2bb7ae17a787923e6d8691974a0b8701d1a2b2
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-12-15 19:16:07 +0200

    test: refactor specs to use FactoryBot

--- spec/controllers/skateparks_controller_spec.rb
+++ spec/controllers/skateparks_controller_spec.rb
@@ -1,48 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe SkateparksController, type: :controller do
+RSpec.describe SkateparksController do
   include ActionView::Helpers::SanitizeHelper
   include Rails.application.routes.url_helpers
 
-  let(:valid_skatepark_attributes) do
-    {
-      name: "Test Skatepark",
-      lat: 12.345,
-      lng: 67.890,
-      description: "<strong>A great skatepark for everyone!</strong>",
-      cover_image: fixture_file_upload('sample_image2.jpg'),
-      images: [
-        fixture_file_upload('sample_image1.jpg'),
-        fixture_file_upload('sample_image3.jpg')
-      ],
-      status: :published
-    }
-  end
+  let!(:skatepark) { create(:skatepark) }
 
-  let!(:skatepark) { Skatepark.create!(valid_skatepark_attributes) }
-
-  describe "GET #index" do
-    it "returns a success response and assigns all skateparks" do
+  describe 'GET #index' do
+    it 'returns a success response and assigns all skateparks' do
       get :index
-      expect(response).to be_successful
-      expect(assigns(:skateparks)).to include(skatepark)
+      aggregate_failures do
+        expect(response).to be_successful
+        expect(assigns(:skateparks)).to include(skatepark)
+      end
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response and assigns the requested skatepark" do
+  describe 'GET #show' do
+    it 'returns a success response' do
       get :show, params: { id: skatepark.id }
-      expect(response).to be_successful
-      expect(assigns(:skatepark)).to eq(skatepark)
-      expect(assigns(:title)).to eq("#{skatepark.name} | Skateparks.gr")
-      expect(assigns(:meta_description)).to eq(skatepark.description.to_plain_text)
-      expect(assigns(:meta_image)).to eq(url_for(skatepark.cover_image))
+      aggregate_failures do
+        expect(response).to be_successful
+        expect(assigns(:skatepark)).to eq(skatepark)
+      end
     end
 
-    it "raises an error if the skatepark does not exist" do
-      expect {
-        get :show, params: { id: "nonexistent-id" }
-      }.to raise_error(ActiveRecord::RecordNotFound)
+    it 'sets meta tags' do
+      get :show, params: { id: skatepark.id }
+      aggregate_failures do
+        expect(assigns(:title)).to eq("#{skatepark.name} | Skateparks.gr")
+        expect(assigns(:meta_description)).to eq(skatepark.description.to_plain_text)
+        expect(assigns(:meta_image)).to eq(url_for(skatepark.cover_image))
+      end
+    end
+
+    it 'raises an error if the skatepark does not exist' do
+      expect do
+        get :show, params: { id: 'nonexistent-id' }
+      end.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
--- spec/models/skatepark_spec.rb
+++ spec/models/skatepark_spec.rb
@@ -4,86 +4,42 @@ RSpec.describe Skatepark, type: :model do
 
   describe "validations" do
     it "requires name to be present" do
-      skatepark = Skatepark.new(cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lat: 12.345,
-                                lng: 67.890,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: :published)
-
+      skatepark = build(:skatepark)
       skatepark.name = nil
       expect(skatepark.save).to be false
     end
 
     it "requires cover_image to be present" do
-      skatepark = Skatepark.new(name: "Test Skatepark",
-                                lat: 12.345,
-                                lng: 67.890,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: :published)
-
+      skatepark = build(:skatepark)
       skatepark.cover_image = nil
       expect(skatepark.save).to be false
     end
 
     it "requires lat to be present" do
-      skatepark = Skatepark.new(name: "Test Skatepark",
-                                cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lng: 67.890,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: :published)
-
+      skatepark = build(:skatepark)
       skatepark.lat = nil
       expect(skatepark.save).to be false
     end
 
     it "requires lng to be present" do
-      skatepark = Skatepark.new(name: "Test Skatepark",
-                                cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lat: 12.345,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: :published)
-
+      skatepark = build(:skatepark)
       skatepark.lng = nil
       expect(skatepark.save).to be false
     end
 
     it "requires description to be present" do
-      skatepark = Skatepark.new(name: "Test Skatepark",
-                                cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lat: 12.345,
-                                lng: 67.890,
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: :published)
-
+      skatepark = build(:skatepark)
       skatepark.description = nil
       expect(skatepark.save).to be false
     end
 
     it "requires at least 2 images to be present" do
-      skatepark = Skatepark.new(name: "Test Skatepark",
-                                cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lat: 12.345,
-                                lng: 67.890,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg')],
-                                status: :published)
-
+      skatepark = build(:skatepark, images: [fixture_file_upload('sample_image1.jpg')])
       expect(skatepark.save).to be false
     end
 
     it "requires status to be present and valid" do
-      skatepark = Skatepark.new(name: "Test Skatepark",
-                                cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lat: 12.345,
-                                lng: 67.890,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: nil)
-
+      skatepark = build(:skatepark, status: nil)
       expect(skatepark.save).to be false
 
       skatepark.status = :published
@@ -91,41 +47,24 @@ RSpec.describe Skatepark, type: :model do
     end
 
     it "generates a slug based on the name" do
-      skatepark = Skatepark.new(name_en: "Test Skatepark",
-                                name_el: "Test Skatepark",
-                                cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lat: 12.345,
-                                lng: 67.890,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: :published)
+      skatepark = build(:skatepark, name_en: "Test Skatepark", name_el: "Test Skatepark")
       skatepark.save
       expect(skatepark.slug).to eq("test-skatepark")
     end
 
     it "updates the slug when the name changes" do
-      skatepark = Skatepark.new(name_en: "Test Skatepark",
-                                name_el: "Test Skatepark",
-                                cover_image: fixture_file_upload('sample_image2.jpg'),
-                                lat: 12.345,
-                                lng: 67.890,
-                                description: "Skatepark description",
-                                images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                status: :published)
-      skatepark.save
+      skatepark = create(:skatepark, name_en: "Test Skatepark", name_el: "Test Skatepark")
       skatepark.update(name_en: "Updated Name")
       expect(skatepark.slug).to eq("updated-name")
     end
 
     it "returns the correct format in to_param" do
-      skatepark = Skatepark.create!(name_el: "Chicago Skate Park",
-                                    name_en: "Chicago Skate Park",
-                                    cover_image: fixture_file_upload('sample_image2.jpg'),
-                                    lat: 41.8819,
-                                    lng: -87.6231,
-                                    description: "Another skatepark option",
-                                    images: [fixture_file_upload('sample_image1.jpg'), fixture_file_upload('sample_image3.jpg')],
-                                    status: :published)
+      skatepark = create(:skatepark, :us_location,
+                         name_en: "Chicago Skate Park",
+                         name_el: "Chicago Skate Park",
+                         state: 'IL',
+                         lat: 41.8819,
+                         lng: -87.6231)
 
       expect(skatepark.to_param).to eq("#{skatepark.id}-chicago-skate-park")
     end
