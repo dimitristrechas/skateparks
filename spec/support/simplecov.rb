c7984109f858b4bae3d7c7b3f27f946a47344a0f
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-12-25 22:51:32 +0200

    fix: simplecov in GitHub actions

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -1,9 +1,11 @@
 require 'simplecov'
 
 SimpleCov.start 'rails' do
+  enable_coverage :branch
   add_filter '/spec/'
   add_filter '/config/'
   add_filter '/vendor/'
+  add_filter '/db/'
 
   add_group 'Models', 'app/models'
   add_group 'Controllers', 'app/controllers'
