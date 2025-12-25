f06cc65cb6f7324d5cf86f0eba3b231ec749d7e9
Dimitrios Trechas <dimitristrechas@gmail.com>
2025-12-25 19:33:40 +0200

    refactor: simplify locale helper methods

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -5,23 +5,24 @@ module LocaleHelper
   }.freeze
 
   def locale_selector
-    active_locale = I18n.locale
-
     content_tag :div, class: 'relative' do
-      elements = [
-        content_tag(:div, LOCALES[active_locale], class: 'cursor-pointer'),
-      ]
-      elements << content_tag(:ul, class: 'absolute inset-x-0 bottom-0 bg-white') do
-        links = []
-        I18n.available_locales.each do |locale|
-          link = link_to LOCALES[locale],
-                         { locale: locale, country_code: params[:country_code], state: params[:state],
-                           page: params[:page], }.compact
-          links << content_tag(:li, link)
-        end
-        links.join.html_safe
-      end
-      elements.join.html_safe
+      safe_join([
+                  content_tag(:div, LOCALES[I18n.locale], class: 'cursor-pointer'),
+                  locale_list,
+                ])
     end
+  end
+
+  private
+
+  def locale_list
+    content_tag(:ul, class: 'absolute inset-x-0 bottom-0 bg-white') do
+      safe_join(I18n.available_locales.map { |locale| content_tag(:li, locale_link(locale)) })
+    end
+  end
+
+  def locale_link(locale)
+    link_to LOCALES[locale],
+            { locale: locale, country_code: params[:country_code], state: params[:state], page: params[:page] }.compact
   end
 end
