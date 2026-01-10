9e649d3e262a2066ac11e9041a200598e8df0b4d
Dimitrios Trechas <dimitristrechas@gmail.com>
2026-01-10 21:19:07 +0200

    feat: Introduce LinkButtonComponent and improve focus colors in homepage

---
+++ app/components/link_button_component.rb
@@ -0,0 +1,25 @@
+class LinkButtonComponent < ViewComponent::Base
+  def initialize(title:, url:, classnames: nil, target: '_self', active: false)
+    super
+    @title = title
+    @url = url
+    @classnames = classnames
+    @target = target
+    @active = active
+  end
+
+  private
+
+  def base_classes
+    'rounded-lg py-2 px-4 focus:outline-2 focus:outline-offset-2 ' \
+      'focus:outline-neutral-700 dark:focus:outline-neutral-200'
+  end
+
+  def state_classes
+    if @active
+      'outline outline-gray-200 bg-gray-200 dark:outline-gray-800 dark:bg-gray-800'
+    else
+      'hover:outline hover:outline-gray-200 hover:bg-gray-200 dark:hover:outline-gray-800 dark:hover:bg-gray-800'
+    end
+  end
+end
